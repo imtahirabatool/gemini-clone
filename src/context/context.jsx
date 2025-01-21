c09f034df9e2119f3245470407caf406a5b299aa
Nabia Noor <mtariq2270@gmail.com>
2025-01-21 15:46:51 +0500

    first commit

---
+++ src/context/context.jsx
@@ -0,0 +1,38 @@
+import { createContext, useState } from "react";
+import runChat from "../config/gemini";
+
+export const Context = createContext();
+
+const ContextProvider = ({ children }) => {
+  const [input, setInput] = useState("");
+  const [recentPrompt, setRecentPrompt] = useState("");
+  const [prevPrompts, setPrevPrompts] = useState([]);
+  const [showResult, setShowResult] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [resultData, setResultData] = useState("");
+
+  const onSent = async (prompt) => {
+    try {
+      await runChat(prompt);
+    } catch (error) {
+      console.error("Error sending prompt:", error);
+    }
+  };
+
+  const contextValue = {
+    prevPrompts,
+    setPrevPrompts,
+    onSent,
+    setRecentPrompt,
+    recentPrompt,
+    showResult,
+    loading,
+    resultData,
+    input,
+    setInput,
+  };
+
+  return <Context.Provider value={contextValue}>{children}</Context.Provider>;
+};
+
+export default ContextProvider;
